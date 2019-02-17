57f72404bcb6906490145a5b7dbfe83169a70aa4
Meg Gutshall <meghan.gutshall@gmail.com>
2019-02-16 23:38:45 -0500

    Fix date format

--- db/seeds/2-medications.rb
+++ db/seeds/2-medications.rb
@@ -5,14 +5,14 @@
 # Clear any existing medications from the database
 Medication.destroy_all
 
-# Create new medications
+# Create new medications for Sam
 Medication.create!(
   name: "Prozac",
   dosage_amount: "25 mg",
   dosage_form: "caplets",
   usage_instructions: "Take one daily",
   prescribing_doctor: "Dr. Johnson",
-  start_date: "2010-12",
+  start_date: "2010-12-01",
   end_date: "",
   ailment_treated: "anxiety",
   user_id: "1"
@@ -23,19 +23,21 @@ Medication.create!(
   dosage_form: "tablet",
   usage_instructions: "Take once daily for 5 days",
   prescribing_doctor: "Dr. Heinbert",
-  start_date: "2018-02",
-  end_date: "2018-03",
+  start_date: "2018-02-01",
+  end_date: "2018-03-01",
   ailment_treated: "tonsillectomy",
   user_id: "1"
 )
+
+# Create new medications for Beth
 Medication.create!(
   name: "Wellbutrin",
   dosage_amount: "150 mg",
   dosage_form: "capsules",
   usage_instructions: "Take once daily before bed",
   prescribing_doctor: "Dr. Shanastay",
-  start_date: "2016-05",
-  end_date: "YYYY-MM",
+  start_date: "2016-05-01",
+  end_date: "",
   ailment_treated: "depression",
   user_id: "2"
 )
@@ -45,8 +47,8 @@ Medication.create!(
   dosage_form: "caplets",
   usage_instructions: "Take once daily before bed",
   prescribing_doctor: "Dr. Shanastay",
-  start_date: "2016-05",
-  end_date: "YYYY-MM",
+  start_date: "2016-05-01",
+  end_date: "",
   ailment_treated: "depression",
   user_id: "2"
 )
@@ -56,7 +58,7 @@ Medication.create!(
   dosage_form: "capsule",
   usage_instructions: "Take once daily",
   prescribing_doctor: "",
-  start_date: "2001-07",
+  start_date: "2001-07-01",
   end_date: "",
   ailment_treated: "ADHD",
   user_id: "2"
@@ -67,18 +69,20 @@ Medication.create!(
   dosage_form: "pill",
   usage_instructions: "Take once daily",
   prescribing_doctor: "",
-  start_date: "2004-10",
-  end_date: "",
+  start_date: "2004-10-01",
+  end_date: "2014-09-01",
   ailment_treated: "birth control",
   user_id: "2"
 )
+
+# Create new medications for Charles
 Medication.create!(
   name: "Levatol",
   dosage_amount: "20 mg",
   dosage_form: "tablet",
   usage_instructions: "Take once daily with food",
   prescribing_doctor: "Dr. Leverlitz",
-  start_date: "2017-04",
+  start_date: "2017-04-01",
   end_date: "",
   ailment_treated: "high blood pressure",
   user_id: "3"
@@ -89,7 +93,7 @@ Medication.create!(
   dosage_form: "tablet",
   usage_instructions: "Take once daily",
   prescribing_doctor: "Dr. Leverlitz",
-  start_date: "2017-10",
+  start_date: "2017-10-01",
   end_date: "",
   ailment_treated: "blood clots",
   user_id: "3"
@@ -100,7 +104,7 @@ Medication.create!(
   dosage_form: "pill",
   usage_instructions: "Take as directed on package",
   prescribing_doctor: "Dr. Leverlitz",
-  start_date: "2019-02",
+  start_date: "2019-02-01",
   end_date: "",
   ailment_treated: "flu",
   user_id: "3"
