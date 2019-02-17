98861f368df13cdc98f069575dc238e8a2168a60
Meg Gutshall <meghan.gutshall@gmail.com>
2019-02-17 14:33:04 -0500

    Remove current validation

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,6 +1,6 @@
 class Reaction < ActiveRecord::Base
   # Add validation helpers here
-  validates :title, :reaction_detail, :current, presence: true
+  validates :title, :reaction_detail, presence: true
 
   # Add associations here
   belongs_to :medication
