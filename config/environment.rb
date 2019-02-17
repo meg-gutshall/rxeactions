31f0ff17d97dc4c44a70cd6ed2652dcabc76b2a4
Meg Gutshall <meghan.gutshall@gmail.com>
2019-02-16 19:52:51 -0500

    Delete unneeded lines of code

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,8 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  adapter: "sqlite3",
+  database: "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
-require './app/controllers/application_controller'
 require_all 'app'
