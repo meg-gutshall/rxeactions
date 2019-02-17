3f7dec4439d16d40d51e059ddc6127f7a169479a
Meg Gutshall <meghan.gutshall@gmail.com>
2019-02-16 23:38:59 -0500

    Stub out seed file

---
+++ db/seeds/3-reactions.rb
@@ -0,0 +1,120 @@
+# My first go at creating seeds for a database
+
+# ==================== Reaction seeds ==================== #
+
+# Clear any existing reactions from the database
+Reaction.destroy_all
+
+# Create new reactions for Prozac
+Reaction.create!(
+  title: "",
+  good_reaction: "",
+  bad_reaction: "",
+  current: true,
+  medication_id: 9
+)
+Reaction.create!(
+  title: "",
+  good_reaction: "",
+  bad_reaction: "",
+  current: true,
+  medication_id: 9
+)
+
+# Create new reactions for Codeine
+Reaction.create!(
+  title: "",
+  good_reaction: "",
+  bad_reaction: "",
+  current: false,
+  medication_id: 10
+)
+
+# Create new reactions for Wellbutrin
+Reaction.create!(
+  title: "",
+  good_reaction: "",
+  bad_reaction: "",
+  current: true,
+  medication_id: 11
+)
+Reaction.create!(
+  title: "",
+  good_reaction: "",
+  bad_reaction: "",
+  current: true,
+  medication_id: 11
+)
+
+# Create new reactions for Lexapro
+Reaction.create!(
+  title: "",
+  good_reaction: "",
+  bad_reaction: "",
+  current: true,
+  medication_id: 12
+)
+
+# Create new reactions for Adderall
+Reaction.create!(
+  title: "",
+  good_reaction: "",
+  bad_reaction: "",
+  current: true,
+  medication_id: 13
+)
+Reaction.create!(
+  title: "",
+  good_reaction: "",
+  bad_reaction: "",
+  current: true,
+  medication_id: 13
+)
+Reaction.create!(
+  title: "",
+  good_reaction: "",
+  bad_reaction: "",
+  current: true,
+  medication_id: 13
+)
+
+# Create new reactions for Yaz
+Reaction.create!(
+  title: "",
+  good_reaction: "",
+  bad_reaction: "",
+  current: false,
+  medication_id: 14
+)
+Reaction.create!(
+  title: "",
+  good_reaction: "",
+  bad_reaction: "",
+  current: false,
+  medication_id: 14
+)
+
+# Create new reactions for Levatol
+Reaction.create!(
+  title: "",
+  good_reaction: "",
+  bad_reaction: "",
+  current: true,
+  medication_id: 15
+)
+
+# Create new reactions for Warfarin
+Reaction.create!(
+  title: "",
+  good_reaction: "",
+  bad_reaction: "",
+  current: true,
+  medication_id: 16
+)
+Reaction.create!(
+  title: "",
+  good_reaction: "",
+  bad_reaction: "",
+  current: true,
+  medication_id: 16
+)
