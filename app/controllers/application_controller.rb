1c2baef3be9bfe0ae0bc5b477866a652a93dfe28
Meg Gutshall <meghan.gutshall@gmail.com>
2019-02-17 20:09:03 -0500

    Create application_controller
    In the configure block, I enabled the use of sessions and set the
    session secret by using a gem called Sysrandom to encode the user's
    password. I also set other app configurations.
    
    I created two helper methods: one to identify the current user and
    another to identify if the user is logged in or not.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,31 @@
 require './config/environment'
+require 'sysrandom/securerandom'
 
 class ApplicationController < Sinatra::Base
 
   configure do
+    # Enable the use of sessions and use the Sysrandom gem to set the session secret
+    enable :sessions
+    set :session_secret, ENV.fetch('SESSION_SECRET') {SecureRandom.hex(64)}
+    # Set app configurations
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
   get "/" do
-    erb :welcome
+    erb :corneal
+  end
+
+  ## ========== HELPER METHODS ========== ##
+
+  # Returns the current user or `nil` if none exists
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  # Returns true or `nil` if there is no current user
+  def logged_in?
+    !!current_user
   end
 
 end
