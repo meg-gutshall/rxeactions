59e519fab1a22d78fca4ab75ee7c67abbccaa0bd
Meg Gutshall <meghan.gutshall@gmail.com>
2019-02-21 15:23:17 -0500

    Create current_med helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,14 +17,19 @@ class ApplicationController < Sinatra::Base
 
   ## ========== HELPER METHODS ========== ##
 
-  # Returns the current user or `nil` if none exists
+  # Returns the current user or 'nil' if none exists
   def current_user
     User.find_by(id: session[:user_id])
   end
 
-  # Returns true or `nil` if there is no current user
+  # Returns true or 'nil' if there is no current user
   def logged_in?
     !!current_user
+  end
+
+  # Returns the current medication of 'nil' if none exists
+  def current_med
+    Medication.find_by(id: session[:medication_id])
   end
 
 end
