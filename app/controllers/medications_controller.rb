4ae1f1030e2ec1509ee9f786d86b26b374e898be
Meg Gutshall <meghan.gutshall@gmail.com>
2019-02-21 16:45:03 -0500

    Get medication sessions to work

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -1,34 +1,40 @@
 # TODO: Add user and login validations for each route
+
 class MedicationsController < ApplicationController
 
-  # List medications
+  # List medications by user
   get '/medications' do
     @med = Medication.all.select{|m| m.user_id == session[:user_id]}
-    session[:medication_id] = @med.id
     erb :'/medications/list_medications'
   end
 
+  # Render the new medication form
   get '/medications/new' do
     erb :'/medications/new_medication'
   end
 
+  # Create a new medication based on the input collected from the user
   post '/medications' do
     @med = Medication.create(params[:med])
     session[:medication_id] = @med.id
-    redirect "/medications"
+    redirect "/medications/#{@med.slug}"
   end
 
+  # Show medication details
   get '/medications/:slug' do
     @med = Medication.find_by_slug(params[:slug])
     session[:medication_id] = @med.id
     erb :'/medications/medication_detail'
   end
 
+  # Render the edit medication form
   get '/medications/:slug/edit' do
     @med = Medication.find_by_slug(params[:slug])
+    session[:medication_id] = @med.id
     erb :'/medications/edit_medication'
   end
 
+  # Edit a reaction based on the input collected from the user
   patch '/medications/:slug' do
     @med = Medication.find_by_slug(params[:slug])
     @med.update(params[:med])
@@ -36,9 +42,10 @@ class MedicationsController < ApplicationController
     redirect "/medications/:slug"
   end
 
-  # Delete medication
+  # Delete a medication
   delete '/medications/:slug' do
     @med = Medication.find_by_slug(params[:slug])
+    session[:medication_id] = @med.id
     @med.delete
   end
 
