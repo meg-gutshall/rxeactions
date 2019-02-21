024412e2e1883741371e6acc52b73ecddfcdc749
Meg Gutshall <meghan.gutshall@gmail.com>
2019-02-21 16:45:29 -0500

    Build out controller and add comments

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,39 +1,49 @@
 # TODO: Add user and login validations for each route
+# Must go to medication view page to create a new reaction!!
+
 class ReactionsController < ApplicationController
 
-  # List reactions
+  # List reactions by medication
   get '/reactions' do
-    # Build helper method
-    @reax = Reaction.all.select{|r| r.user_id == session[:user_id]}
+    @reax = current_med.reactions
     erb :'/reactions/list_reactions'
   end
 
+  # Render the new reaction form
   get '/reactions/new' do
     erb :'/reactions/new_reaction'
   end
 
+  # Create a new reaction based on the input collected from the user
   post '/reactions' do
     @reax = Reaction.create(params[:reax])
-    redirect "/reactions"
+    # Validate input
+    if current_med.id != @reax.medication_id
+      redirect # where??
+    end
+    redirect "/reactions/#{@reax.slug}"
   end
 
+  # Show reaction details
   get '/reactions/:slug' do
     @reax = Reaction.find_by_slug(params[:slug])
     erb :'/reactions/reaction_detail'
   end
 
+  # Render the edit reaction form
   get '/reactions/:slug/edit' do
     @reax = Reaction.find_by_slug(params[:slug])
     erb :'/reactions/edit_reaction'
   end
-
+  
+  # Edit a reaction based on the input collected from the user
   patch '/reactions/:slug' do
     @reax = Reaction.find_by_slug(params[:slug])
     @reax.update(params[:reax])
     redirect "/reactions/:slug"
   end
 
-  # Delete reaction
+  # Delete a reaction
   delete '/reactions/:slug' do
     @reax = Reaction.find_by_slug(params[:slug])
     @reax.delete
