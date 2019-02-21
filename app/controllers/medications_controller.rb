6ae8e2372c06ab427b4df52bd5b02582ecb4fa96
Meg Gutshall <meghan.gutshall@gmail.com>
2019-02-20 20:36:23 -0500

    Build out MedicationsController

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -1,3 +1,40 @@
+# TODO: Add user and login validations for each route
 class MedicationsController < ApplicationController
+
+  # List medications
+  get '/medications' do
+    erb :'/medications/list_medications'
+  end
+
+  get '/medications/new' do
+    erb :'/medications/new_medication'
+  end
+
+  post '/medications' do
+    @med = Medication.create(params[:med])
+    redirect "/medications"
+  end
+
+  get '/medications/:slug' do
+    @med = Medication.find_by_slug(params[:slug])
+    erb :'/medications/medication_detail'
+  end
+
+  get '/medications/:slug/edit' do
+    @med = Medication.find_by_slug(params[:slug])
+    erb :'/medications/edit_medication'
+  end
+
+  patch '/medications/:slug' do
+    @med = Medication.find_by_slug(params[:slug])
+    @med.update(params[:med])
+    redirect "/medications/:slug"
+  end
+
+  # Delete medication
+  delete '/medications/:slug' do
+    @med = Medication.find_by_slug(params[:slug])
+    @med.delete
+  end
 
 end
