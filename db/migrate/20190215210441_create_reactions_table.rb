5a383ffc784a9e6be01005d677aa01a497c273de
Meg Gutshall <meghan.gutshall@gmail.com>
2019-02-22 20:09:50 -0500

    Add reaction attributes

--- db/migrate/20190215210441_create_reactions_table.rb
+++ db/migrate/20190215210441_create_reactions_table.rb
@@ -3,6 +3,9 @@ class CreateReactionsTable < ActiveRecord::Migration[5.2]
     create_table :reactions do |t|
       t.string :title
       t.text :reaction_detail
+      t.string :dosage_amount
+      t.string :dosage_form
+      t.text :usage_instructions
       t.integer :medication_id
       t.timestamps
     end
