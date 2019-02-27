51241e959b83f49c402485dcced8dcdccdf8d461
Meg Gutshall <meghan.gutshall@gmail.com>
2019-02-27 16:59:55 -0500

    Remove reaction slug methods

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -4,13 +4,4 @@ class Reaction < ActiveRecord::Base
 
   # Add associations here
   belongs_to :medication
-
-  # Create slug methods
-  def slug
-    "reaction-#{id}"
-  end
-
-  def self.find_by_slug(slug)
-    self.all.find {|r| r.slug == slug}
-  end
 end
