3f79c5ff58e1eed3c75a2d491d9c97cd6918300d
Meg Gutshall <meghan.gutshall@gmail.com>
2019-03-01 22:09:35 -0500

    Comment out old Rake task for new seeding method

--- Rakefile
+++ Rakefile
@@ -10,14 +10,14 @@ task :console do
 end
 
 # Creates a new Rake task that seeds the database using custom seed files
-namespace :db do
-  namespace :seed do
-    Dir[File.join(File.dirname(__FILE__), "db/seeds", "*.rb")].each do |filename|
-      task_name = File.basename(filename, '.rb')
-      desc "Seed " + task_name + ", based on the file with the same name in `db/seeds/*.rb`"
-      task task_name.to_sym => :environment do
-        load(filename) if File.exist?(filename)
-      end
-    end
-  end
-end
+# namespace :db do
+#   namespace :seed do
+#     Dir[File.join(File.dirname(__FILE__), "db/seeds", "*.rb")].each do |filename|
+#       task_name = File.basename(filename, '.rb')
+#       desc "Seed " + task_name + ", based on the file with the same name in `db/seeds/*.rb`"
+#       task task_name.to_sym => :environment do
+#         load(filename) if File.exist?(filename)
+#       end
+#     end
+#   end
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,7 @@
-# # ========== Create user seeds ========== #
+require 'pathname'
 
-# # Clear any existing users from the database
-# User.destroy_all
+root_path = Pathname.new('..').expand_path(File.dirname(__FILE__))
 
-# # User seeds information
-# user_info = [
-#   [name: "Sam Carver", email: "sammy@gmail.com", password: "password", birth_date: "1990-01-01", known_allergies: "Aspirin"],
-#   [name: "Beth Horne", email: "betsy@aol.com", password: "password", birth_date: "1983-04-13", known_allergies: ""],
-#   [name: "Charles Planch", email: "trombonez33@gmail.com", password: "password", birth_date: "1975-11-25", known_allergies: "Penicillin"]
-# ]
-
-# # Create new users
-# user_info.each do |name, email, password, birth_date, known_allergies|
-#   User.create!(name: name, email: email, password: password, birth_date: birth_date, known_allergies: known_allergies)
-# end
-
-Dir[File.join(File.dirname(__FILE__), 'db', 'seeds', '*.rb')].sort.each do |seed|
+Dir[File.join(root_path, "db", "seeds", "*.rb")].sort.each do |seed|
   load seed
 end
