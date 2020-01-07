bf4ec1dbdfa10a3466ce66bf01502061b0d620d8
Meg Gutshall <meghan.gutshall@gmail.com>
2020-01-07 17:55:44 -0500

    Add postgres credentials

--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,25 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 # Establish connection with the database
-ActiveRecord::Base.establish_connection(
-  adapter: "sqlite3",
-  database: "db/#{ENV['SINATRA_ENV']}.sqlite"
+configure :development do
+  ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
+end
+
+configure :production do
+  db = URI.parse(ENV['HEROKU_POSTGRESQL_COBALT_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
 
 # Require ApplicationController before all other files
 require './app/controllers/application_controller'
