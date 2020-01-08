9c643ffd2990f7123e007f30251d6e875f11f0c9
Meg Gutshall <meghan.gutshall@gmail.com>
2020-01-07 22:34:49 -0500

    Remove sqlite from config files

--- config/environment.rb
+++ config/environment.rb
@@ -3,28 +3,19 @@ ENV['SINATRA_ENV'] ||= "development"
 
 # Require Gemfile and bundle gems with dependencies
 require 'bundler/setup'
-# Bundler.require(:default, ENV['SINATRA_ENV'])
+Bundler.require(:default, ENV['SINATRA_ENV'])
 
 # Establish connection with the database
-configure :development do
-  ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+ActiveRecord::Base.establish_connection(
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
 )
-end
-
-configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-  )
-end
 
 # Require ApplicationController before all other files
 require './app/controllers/application_controller'
